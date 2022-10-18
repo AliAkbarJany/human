ce9c860522790427e3f48ef49df60dcf3fde80ae
KhanAnupamShafi <deep71.bd@gmail.com>
2022-10-18 15:31:09 +0600

    App js updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import Navbar from './Shared/Navbar/Navbar';
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./Shared/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <h2>hello rafsan</h2>
+      <h2>hello rafsan and Aupam</h2>
+      <p>I added this line</p>
+      <del>Ignore this</del>
       <Navbar></Navbar>
       <Routes>
         <Route></Route>
